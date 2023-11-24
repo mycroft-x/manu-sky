66b3d9ccde57b48c6ef76162a303698a5cbd4d34
Manu <manuchimsoazubuike@gmail.com>
2023-11-24 21:20:18 +0100

    woop

--- weather by manu/js/index.js
+++ weather by manu/js/index.js
@@ -1,10 +1,13 @@
 const menuBtn = document.getElementById('menu-btn')
 const navBar = document.getElementById('nav-bar')
+const middle = document.getElementById('home-middle')
 
 menuBtn.addEventListener('click', menuExpand)
 
-let expanded = 1
+const dawn = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+const dusk = [18, 19, 20, 21, 22, 23, 1, 2, 3, 4, 5]
 
+let expanded = 1
 function menuExpand(params) {
     if (expanded == 1) {
         navBar.style.cssText="width: 100px"
@@ -13,5 +16,17 @@ function menuExpand(params) {
         navBar.style.cssText="width: max-content"
         expanded = expanded *-1        
     }
-    
 }
+
+// middle.style.background="url('/img/images/night.jpg')"
+
+
+if (dawn.includes(Date)){
+    middle.style.background = "url('/img/images/day.jpg')"
+} else {
+    middle.style.background = "url('/img/images/night.jpg')"
+}  
+
+
+theming()
+
---
+++ weather by manu/js/test.js
@@ -0,0 +1,13 @@
+console.log(Date());
+
+d = new Date()
+d.getMonth()
+console.log(d.getMonth());
+
+
+const dawn = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+const dusk = [18, 19, 20, 21, 22, 23, 1, 2, 3, 4, 5]
+
+console.log(dawn.includes(d.getHours()))
+
+f = d.getHours()
