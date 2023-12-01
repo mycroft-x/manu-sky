14904bd1a0ed50e6fc977becf79816ae98fe79a0
Manu <manuchimsoazubuike@gmail.com>
2023-12-02 00:37:30 +0100

    theming test again

--- theming.js
+++ theming.js
@@ -3,10 +3,12 @@ const middle = document.getElementById('home-middle')
 
 export function theming(params) {
     if (dawn.includes(currTime.getHours())== true){
-        middle.style.background = "url('/cloudss.png') no-repeat"
+        middle.style.backgroundImage = "url('/cloudss.png') no-repeat"
         middle.style.backgroundSize = "cover"
     }else if (dusk.includes(currTime.getHours()) == true){
-        middle.style.background = "url('/night.jpeg')"
+        middle.style.backgroundImage = "url('night.jpeg')"
         middle.style.backgroundSize = "cover"
     }  
 }
+
+theming()
